7052e6d5dcb72b0a8442e9cc67c82918a10bab32
Akash <akash.samantra19@gmail.com>
2018-06-17 13:33:14 +0530

    Minor bug fixes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,39 +3,29 @@ import { LOAD_DECKS, ADD_DECK, ADD_CARD } from '../actions'
 function decks(state = {}, action) {
   switch (action.type) {
     case LOAD_DECKS:
-      {
-        return {
-          ...state,
-          ...action.decks
-        }
-        break;
+      return {
+        ...state,
+        ...action.decks
       }
 
     case ADD_DECK:
-      {
-        const o = {
-          ...state,
-          [action.deckTitle]: {
-            title: action.deckTitle,
-            questions: []
-          }
+      return {
+        ...state,
+        [action.deckTitle]: {
+          title: action.deckTitle,
+          questions: []
         }
-        return o
-        break;
       }
 
     case ADD_CARD:
-      {
-        const out = {
-          ...state
-        }
-        if (out[action.deckTitle]) {
-          const { question, answer } = action.card
-          out[action.deckTitle].questions.push({ question, answer })
-        }
-        return out
-        break;
+      const out = {
+        ...state
       }
+      if (out[action.deckTitle]) {
+        const { question, answer } = action.card
+        out[action.deckTitle].questions.push({ question, answer })
+      }
+      return out
 
     default:
       return state
